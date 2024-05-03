79cca18f916de9f7ae6af61fc49f374d5e94925f
v-sdutta2 <sourav.dutta2@alaskaair.com>
2024-05-03 19:13:19 +0530

    CAP018_BKG_00001 scenario added

---
+++ StepDefinitions/CAP018_BKG_00001_Create_a_booking_StepDefinitions.cs
@@ -0,0 +1,86 @@
+using iCargoUIAutomation.pages;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace iCargoUIAutomation.StepDefinitions
+{
+    [Binding]
+    public class CAP018_BKG_00001_Create_a_booking_StepDefinitions : BasePage
+    {
+        private IWebDriver driver;
+        private PageObjectManager pageObjectManager;
+        private homePage hp;
+        private MaintainBookingPage mbp;
+        string origin = "";
+        string destination = "";       
+        string productCode = "";
+        string commodity = "";
+        string piece = "";
+        string weight = "";
+        string flightno = "";
+        string flightDate = "";
+
+        public CAP018_BKG_00001_Create_a_booking_StepDefinitions(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+            pageObjectManager = new PageObjectManager(driver);
+            hp = pageObjectManager.GetHomePage();
+            mbp = pageObjectManager.GetMaintainBookingPage();
+        }
+
+        [When(@"User enters screen name as '([^']*)'")]
+        public void WhenUserEntersScreenNameAs(string screenName)
+        {
+            hp.enterScreenName(screenName);
+            mbp.SwitchToCAP018Frame();
+        }
+
+        [Then(@"User clicks on New/List button")]
+        public void ThenUserClicksOnNewListButton()
+        {
+           
+            mbp.ClickNewListButton();
+        }
+
+        [Then(@"User enters shipment details with Origin ""([^""]*)"", Destination ""([^""]*)"", Product Code ""([^""]*)""")]
+        public void ThenUserEntersShipmentDetailsWithOriginDestinationShippingDateProductCode(string origin, string destination, string productCode)
+        {
+            this.origin = origin;
+            this.destination = destination;            
+            this.productCode = productCode;
+            mbp.EnterShipmentDetails(origin, destination,productCode);
+        }
+
+        [Then(@"User enters Shipper and Consignee details")]
+        public void ThenUserEntersShipperAndConsigneeDetails()
+        {
+            mbp.EnterShipperConsigneeDetails();
+        }
+
+        [Then(@"User enters commodity details with Commodity ""([^""]*)"", Pieces ""([^""]*)"", Weight ""([^""]*)""")]
+        public void ThenUserEntersCommodityDetailsWithCommodityPiecesWeight(string commodity, string piece, string weight)
+        {
+            this.commodity = commodity;
+            this.piece = piece;
+            this.weight = weight;
+            mbp.EnterCommodityDetails(commodity, piece, weight);
+        }
+
+       
+
+        [Then(@"User clicks on Save button")]
+        public void ThenUserClicksOnSaveButton()
+        {
+            mbp.ClickSaveButton();            
+        }
+
+       
+        [Then(@"User selects flight for ""([^""]*)""")]
+        public void ThenUserSelectsFlightFor(string productCode)
+        {
+            mbp.SelectFlight(productCode);
+        }
+
+    }
+}
---
+++ pages/PageObjectManager.cs
@@ -0,0 +1,33 @@
+using iCargoUIAutomation.pages;
+using OpenQA.Selenium;
+
+public class PageObjectManager : BasePage
+{
+
+    private IWebDriver driver;
+    private homePage hp;    
+    private MaintainBookingPage mbp;
+
+    // Add other page classes as needed
+
+    public PageObjectManager(IWebDriver driver) : base(driver)
+    {
+        this.driver = driver;
+    }
+
+    public homePage GetHomePage()
+    {
+        return hp ?? (hp = new homePage(driver));
+    }
+
+    
+
+    public MaintainBookingPage GetMaintainBookingPage()
+    {
+        return mbp ?? (mbp = new MaintainBookingPage(driver));
+    }
+
+
+
+    // Add other getter methods for other page classes as needed
+}
