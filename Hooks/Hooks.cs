9f953500b8ada29ea0473206832449df9b27f8a9
Avijit Saha <avijit.saha@alaskaair.com>
2024-11-06 13:54:24 +0530

    Change in hooks

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -130,7 +130,7 @@ namespace iCargoUIAutomation.Hooks
             hp.logoutiCargo();
             extent.Flush();
             azureStorage = new AzureStorage(reportContainerName);
-            //azureStorage.UploadFolderToAzure(reportPath);
+            azureStorage.UploadFolderToAzure(reportPath);
             foreach (string blobPath in uploadedBlobPaths)
             {                
                TestContext.WriteLine($"Blob file path: {blobPath}");
@@ -217,7 +217,7 @@ namespace iCargoUIAutomation.Hooks
                 }                
 
                 // Upload the updated file back to Azure Blob Storage
-               //azureStorage.UploadFileToBlob(tempLocalPath, excelFileName);
+               azureStorage.UploadFileToBlob(tempLocalPath, excelFileName);
 
                 File.Delete(tempLocalPath);
             }
