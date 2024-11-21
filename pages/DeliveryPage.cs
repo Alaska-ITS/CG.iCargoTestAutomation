766fcca446a1da788644045b97680e47dbb4fbe4
Avijit Saha <avijit.saha@alaskaair.com>
2024-11-21 17:15:33 +0530

    OPR293_DLV_00001 scenario added

---
+++ pages/DeliveryPage.cs
@@ -0,0 +1,300 @@
+﻿using AventStack.ExtentReports;
+using log4net;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCargoUIAutomation.pages
+{
+    public class DeliveryPage:BasePage
+    {
+        private PageObjectManager pageObjectManager;
+        private PaymentPortalPage ppp;
+        private CreateShipmentPage csp;
+        private string totalPaybleAmount = "";
+        private string accountInfoCIDNum = "";
+        string chargeType = "";
+        ILog Log = LogManager.GetLogger(typeof(CreateShipmentPage));
+
+        public DeliveryPage(IWebDriver driver) : base(driver)
+        {
+            this.pageObjectManager = new PageObjectManager(driver);
+            this.ppp = pageObjectManager.GetPaymentPortalPage();
+            this.csp = pageObjectManager.GetCreateShipmentPage();
+        }
+
+        //OPR293 Delivery Screen header
+        private By deliveryScreenOPR293Frame_Xpath = By.XPath("//iframe[@name='iCargoContentFrameOPR293']");
+        private By txtAWBNumber_CssSelector = By.CssSelector("#awb_b");
+        private By btnList_CssSelector = By.CssSelector("#CMP_GENERATECOLLECTIONLIST_ADDTOLIST_BUTTON");
+
+        //Delivery Details and Payment
+        private By chkboxDeliveryAwb_Xpath = By.XPath("//input[@name='subRowId']");
+        private By btnGenerateDN_CssSelector = By.CssSelector("#CMP_GENERATECOLLECTIONLIST_GENERATEGROUPID_BUTTON");
+        private By txtboxDeliveryPaymentCollectCash_CssSelector = By.CssSelector("#CSH_Cashiering_Defaults_ChargeCollect_PaymentAmount");
+        private By btnAuthCode_Xpath = By.XPath("//button[@name='btAddGateway']");
+        private By lblEmbargoDetails_Xpath = By.XPath("//*[text()='Embargo Details']");
+        private By btnContinueEmbargo_Xpath = By.XPath("//*[text()='Embargo Details']//following::button[@id='okBtn']");        
+
+        //Delivery Confirmation
+        private By txtDNDetailsColor_Xpath = By.XPath("//a[@name='DNDetails_lnk']/div[contains(@style,'color:green')]");
+        private By btnAcceptPayment_Xpath = By.XPath("//button[@name='btnAcceptPayment']");
+        private By txtPaymentConfirmation_Xpath = By.XPath("//a[@name='DNDetails_lnk']/div[contains(text(),'Paid')]");
+        private By btnCapturDelivery_Xpath = By.XPath("//button[@name='btnCaptureDelivery']");
+        private By txtBoxDeiveryTo_Xpath = By.XPath("//input[@name='deliveredTo']");
+        private By btnSaveDeliveryDetails_Xpath = By.XPath("//button[@name='btnSave']"); 
+
+        //Payment Details
+        private By txtPleaseCloseTabRetry_Xpath = By.XPath("//*[text()='Please close the tab and retry.']");
+        private By lblAccountInfo_Xpath = By.XPath("//*[text()='Account information']");
+        private By lblTotalAmount_Xpath = By.XPath("(//*[@class='aiBoxTwo'])[2]//label[13]");
+        private By btnDone_Xpath = By.XPath("//*[text()='Done']");
+
+        public void SwitchToOPR293Frame()
+        {
+            try
+            {
+                SwitchToFrame(deliveryScreenOPR293Frame_Xpath);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Switched to OPR293 Frame");
+                Log.Info("Switched to OPR293 Frame");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in SwitchToOPR293Frame: " + e.Message);
+                Log.Error("Error in SwitchToOPR293Frame: " + e.Message);
+            }
+        }
+
+        public void EnterAWBNumberOPR293()
+        {
+            try
+            {
+                EnterText(txtAWBNumber_CssSelector, CreateShipmentPage.awb_num);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Entered AWB Number: " + CreateShipmentPage.awb_num);
+                Log.Info("Entered AWB Number: " + CreateShipmentPage.awb_num);
+                Click(btnList_CssSelector);
+                Log.Info("Clicked on List Button");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in EnterAWBNumberOPR293: " + e.Message);
+                Log.Error("Error in EnterAWBNumberOPR293: " + e.Message);
+            }
+        }
+
+        public void SelectAWBForDelivery()
+        {
+            try
+            {
+                Click(chkboxDeliveryAwb_Xpath);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Selected AWB for Delivery");
+                Log.Info("Selected AWB for Delivery");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in SelectAWBForDelivery: " + e.Message);
+                Log.Error("Error in SelectAWBForDelivery: " + e.Message);
+            }
+        }
+
+        public void ClickGenerateDNButton()
+        {
+            try
+            {
+                Click(btnGenerateDN_CssSelector);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Clicked on Generate DN Button");
+                Log.Info("Clicked on Generate DN Button");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in ClickGenerateDNButton: " + e.Message);
+                Log.Error("Error in ClickGenerateDNButton: " + e.Message);
+            }
+        }   
+        
+        public void GetPaymentAmountValue()
+        {
+            try
+            {
+                SwitchToSecondPopupWindow();
+                ScrollDown();
+                string paymentAmount = GetAttributeValue(txtboxDeliveryPaymentCollectCash_CssSelector, "value");
+                //convert the string to double
+                double paymentAmountDouble = Convert.ToDouble(paymentAmount);
+                Console.WriteLine("Payment Amount: " + paymentAmountDouble);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Payment Amount: " + paymentAmountDouble);
+                Log.Info("Payment Amount: " + paymentAmountDouble);
+                ScrollDown();
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in GetPaymentAmountValue: " + e.Message);
+                Log.Error("Error in GetPaymentAmountValue: " + e.Message);
+            }
+
+        }
+
+        public string ClickOnAddButtonHandlePaymentPortal(string chargeType)
+        {
+            try 
+            { 
+            log.Info("ClickOnSaveButtonHandlePaymentPortal function");
+            int noOfWindowsBefore = GetNumberOfWindowsOpened();
+            Click(btnAuthCode_Xpath);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Clicked on Auth Code Button");
+                Log.Info("Clicked on Auth Code Button");
+            if (IsElementDisplayed(lblEmbargoDetails_Xpath, 1))
+            {
+                Click(btnContinueEmbargo_Xpath);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Clicked on Continue button for Embargo");
+            }
+            WaitForNewWindowToOpen(TimeSpan.FromSeconds(10), noOfWindowsBefore + 1);
+            int noOfWindowsAfter = GetNumberOfWindowsOpened();
+            if (noOfWindowsAfter > noOfWindowsBefore)
+            {
+                SwitchToLastWindow();                
+                if (!IsElementDisplayed(txtPleaseCloseTabRetry_Xpath, 5) || !IsElementDisplayed(lblAccountInfo_Xpath, 5))
+                {
+                    RefreshPage();
+                    Hooks.Hooks.UpdateTest(Status.Pass, "Refreshed Payment Portal");
+                }
+
+                if (IsElementDisplayed(txtPleaseCloseTabRetry_Xpath, 5))
+                {
+                    CloseCurrentWindow();
+                    Hooks.Hooks.UpdateTest(Status.Pass, "Closed Payment Portal Tab & Retrying");
+                }
+                if (chargeType.Equals("CC"))
+                {
+                    ppp.ConfirmManualPayment();
+                    ScrollDown();
+                    WaitForElementToBeVisible(lblTotalAmount_Xpath, TimeSpan.FromSeconds(5));
+                    totalPaybleAmount = GetText(lblTotalAmount_Xpath).Split("$")[1];
+                    Hooks.Hooks.UpdateTest(Status.Pass, "Total Payable Amount is: " + totalPaybleAmount);
+                    ClickOnElementIfEnabled(btnDone_Xpath);
+                    Hooks.Hooks.UpdateTest(Status.Pass, "Clicked on Done Button");
+                    WaitForNewWindowToOpen(TimeSpan.FromSeconds(10), noOfWindowsBefore);                    
+                }                
+                else
+                {
+                    Hooks.Hooks.UpdateTest(Status.Fail, "Invalid Charge Type");
+                }
+                Hooks.Hooks.UpdateTest(Status.Pass, "Back from Payment Portal");
+                SwitchToLastWindow();                
+            }
+            return totalPaybleAmount;
+        }
+         catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in ClickOnSaveButtonHandlePaymentPortal: " + e.Message);
+                Log.Error("Error in ClickOnSaveButtonHandlePaymentPortal: " + e.Message);
+                return totalPaybleAmount;
+            }
+        }
+
+        public void ClickAcceptPaymentButton()
+        {
+            try
+            {
+                ClickOnElementIfPresent(btnAcceptPayment_Xpath);
+                Hooks.Hooks.UpdateTest(Status.Pass, "Clicked on Accept Payment Button");
+                Log.Info("Clicked on Accept Payment Button");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in ClickAcceptPaymentButton: " + e.Message);
+                Log.Error("Error in ClickAcceptPaymentButton: " + e.Message);
+            }
+        }
+
+        public void DeliveryConfirmationDetails()
+        {
+            try
+            {
+                SwitchToPopupWindow();
+                SwitchToOPR293Frame();
+                string paymentConfirmationColor = GetAttributeValue(txtDNDetailsColor_Xpath, "style");
+                string paymentConfirmation = GetText(txtPaymentConfirmation_Xpath);
+                if (paymentConfirmationColor.ToLower().Contains("green") && paymentConfirmation.ToLower().Contains("paid"))
+                {
+                    Hooks.Hooks.UpdateTest(Status.Pass, "DN Details are displayed in Green Color and Paid");
+                    Log.Info("DN Details are displayed in Green Color and Paid");
+                }
+                else
+                {
+                    Hooks.Hooks.UpdateTest(Status.Fail, "DN Details are not displayed in Green Color and Paid");
+                    Log.Error("DN Details are not displayed in Green Color and not Paid");
+                }
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in Delivery Confirmation Details: " + e.Message);
+                Log.Error("Error in Delivery Confirmation Details: " + e.Message);
+            }
+        }
+
+        public void CaptureDeliveryDetails()
+        {
+            try
+            {
+                Click(btnCapturDelivery_Xpath);                
+                Log.Info("Clicked on Capture Delivery Button");
+                EnterText(txtBoxDeiveryTo_Xpath, "Test");                
+                Log.Info("Entered Delivery To Details");
+                ClickOnElementIfPresent(btnSaveDeliveryDetails_Xpath);   
+                Log.Info("Clicked on Save Delivery Details Button");
+                Hooks.Hooks.UpdateTest(Status.Pass,"Delivery Details Captured Successfully");
+            }
+            catch (Exception e)
+            {
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in Capture Delivery Details: " + e.Message);
+                Log.Error("Error in Capture Delivery Details: " + e.Message);
+            }
+        }
+
+        public void DeliveryReceiptWindow()
+        {
+            try
+            {
+                // Ensure the second popup window is available and switch to it
+                SwitchToSecondPopupWindow();
+
+                // Get the page title (if needed for logging or validation)
+                string pageTitle = GetTitle();
+
+                // Define the screenshot file name and directory
+                string screenshotFileName = "DeliveryReceipt.png";
+                string screenshotDirectory = Path.Combine(Hooks.Hooks.reportPath, "Screenshots");
+
+                // Ensure the directory exists before saving the screenshot
+                if (!Directory.Exists(screenshotDirectory))
+                {
+                    Directory.CreateDirectory(screenshotDirectory);
+                }
+
+                // Construct the full path for the screenshot
+                string screenshotPath = Path.Combine(screenshotDirectory, screenshotFileName);
+
+                // Take a screenshot and save it to the specified path
+                Hooks.Hooks.captureScreenshot(screenshotPath);
+
+                // Log the screenshot path in the report
+                Hooks.Hooks.UpdateTest(Status.Info, "Delivery Receipt Window Screenshot: " + screenshotPath);
+
+                // Close the current window
+                CloseCurrentWindow();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions (e.g., window switch failure, screenshot failure)
+                Hooks.Hooks.UpdateTest(Status.Fail, "Error in Delivery Receipt Window: " + ex.Message);
+                throw; // Rethrow the exception if necessary, or log as needed
+            }
+        }
+
+
+    }
+}
