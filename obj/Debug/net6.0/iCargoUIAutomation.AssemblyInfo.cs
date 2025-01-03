ee242242157fcb008c4fd1baf2729fece501db71
Avijit Saha <avijit.saha@alaskaair.com>
2025-01-03 14:04:00 +0530

    Changes in bin folder

--- obj/Debug/net6.0/iCargoUIAutomation.AssemblyInfo.cs
+++ obj/Debug/net6.0/iCargoUIAutomation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iCargoUIAutomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6a8d9d38987ff12b76d19bf85baa3585899d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("iCargoUIAutomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iCargoUIAutomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
