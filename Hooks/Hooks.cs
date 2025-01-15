70b293bfd22eb91ebc69f9e8ebdf0d749f23cc5f
Avijit Saha <avijit.saha@alaskaair.com>
2025-01-15 19:23:35 +0530

    For 100 test data

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -251,7 +251,7 @@ namespace iCargoUIAutomation.Hooks
                 }                
 
                 // Upload the updated file back to Azure Blob Storage
-               //azureStorage.UploadFileToBlob(tempLocalPath, excelFileName);
+               azureStorage.UploadFileToBlob(tempLocalPath, excelFileName);
 
                 File.Delete(tempLocalPath);
                 extent.Flush();
