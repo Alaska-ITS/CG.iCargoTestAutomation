7f2b752d4b88b1b27b0f3ecc515d83c5b4b29c7a
Varsha Kaushal <Varsha.Kaushal@alaskaair.com>
2025-04-16 19:31:54 +0530

    OPR344_EXP_00008_Finalize_a_flight_that_has_cargo_manifested

---
+++ Tests/OPR344/OPR344_EXP_00008_Finalize a flight that has cargo manifested.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iCargoUIAutomation.Fixtures;
+using iCargoUIAutomation.pages;
+using iCargoUIAutomation.utilities;
+using OpenQA.Selenium;
+
+
+
+namespace iCargoUIAutomation.Tests.OPR344
+{
+    public class OPR344_EXP_00008_Finalize_a_flight_that_has_cargo_manifested : IClassFixture<TestFixture>
+    {
+        private readonly IWebDriver driver;
+        private readonly PageObjectManager pageObjectManager;
+        private readonly HomePage hp;
+        private readonly CreateShipmentPage csp;
+        private readonly ExportManifestPage emp;
+        private readonly MarkFlightMovements mfm;
+        private static string totalPaybleAmount;
+
+        public static IEnumerable<object[]> TestData_OPR344_0008 => ExcelFileDataReader.GetData(BasePage.GetTestDataPath("OPR344_ExportManifest_TestData.xlsx"), "OPR344_EXP_00008");
+        public OPR344_EXP_00008_Finalize_a_flight_that_has_cargo_manifested(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+            pageObjectManager = new PageObjectManager(driver);
+            hp = pageObjectManager.GetHomePage();
+            csp = pageObjectManager.GetCreateShipmentPage();
+            emp = pageObjectManager.GetExportManifestPage();
+            mfm = pageObjectManager.GetMarkFlightMovements();
+        }
+
+        [Theory]
+        [Trait("Category", "OPR344")]
+        [Trait("Category", "OPR344_DLV_00008")]
+        [MemberData(nameof(TestData_OPR344_0008))]
+
+        public void Add_charge_codes_to_be_charged_at_delivery(
+           string agentCode, string shipperCode, string consigneeCode, string origin,
+            string destination, string productCode, string scc, string commodity,
+            string shipmentdesc, string serviceCargoClass, string piece,
+            string weight, string chargeType, string modeOfPayment, string awbSectionName, string sp,
+            string cartType)
+        {
+            try
+            {
+
+                Console.WriteLine("🔹 Starting test: OPR293_DLV_00006_Add_charge_codes_to_be_charged_at_delivery");
+                hp.SwitchStation(origin);
+                hp.enterScreenName("LTE001");
+
+                csp.SwitchToLTEContentFrame();
+                csp.ClickOnAwbTextBox();
+                csp.ClickOnListButton();
+                csp.EnterParticipantDetailsAsync(agentCode, shipperCode, consigneeCode);
+                csp.ClickOnContinueParticipantButton();
+                csp.EnterCertificateDetails();
+                csp.ClickOnContinueCertificateButton();
+                csp.EnterShipmentDetails(origin, destination, productCode, scc, commodity, shipmentdesc, serviceCargoClass, piece, weight);
+                csp.ClickOnContinueShipmentButton();
+                csp.ClickOnSelectFlightButton();
+                csp.BookWithSpecificFlightType("Combination");
+                csp.ClickOnContinueFlightDetailsButton();
+                csp.EnterChargeDetails(chargeType, modeOfPayment);
+                csp.ClickOnCalculateChargeButton();
+                csp.ClickingYesOnPopupWarnings("");
+                csp.ClickOnContinueChargeButton();
+                csp.EnterAcceptanceDetails();
+                csp.ClickOnContinueAcceptanceButton();
+                csp.EnterScreeningDetails(1, "Transfer Manifest Verified", "Pass");
+                csp.ClickOnContinueScreeningButton();
+                csp.ClickOnAWBVerifiedCheckbox();
+                (string awb, totalPaybleAmount) = csp.SaveShipmentDetailsAndHandlePopups();
+
+
+                hp.enterScreenName("OPR344");
+
+                emp.SwitchToManifestFrame();
+                emp.ClickOnFlightTextBox();
+                csp.EnterFlightinExportManifest("");
+                csp.EnterFlightDateExportManifest();
+                emp.ClickOnListButton();
+                csp.CreateNewULDCartExportManifest(cartType, destination);
+                csp.FilterOutAWBULDInExportManifest(awbSectionName);
+                emp.clickOnManifestButton();
+                emp.ClosePrintPDFWindow();
+                emp.ValidateAWBStatusInExportManifest("Manifested");
+                emp.CloseOPR344Screen();
+
+                hp.enterScreenName("FLT006");
+
+                mfm.SwitchToFLT006Frame();
+                mfm.EnterFlightDetails();
+                mfm.ClickListButton();
+                mfm.EnterActualArrivalDepartureDetails("Departure");
+                mfm.ClickSaveButton();
+                mfm.ClickCloseButton();
+
+                hp.enterScreenName("OPR344");
+
+                emp.SwitchToManifestFrame();
+                emp.ClickOnFlightTextBox();
+                csp.EnterFlightinExportManifest("");
+                csp.EnterFlightDateExportManifest();
+                emp.ClickOnListButton();
+                emp.CheckFlightStatusForFinalized();
+                emp.CloseOPR344Screen();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("🔴 Exception: " + e.Message);
+                throw;
+            }
+        }
+    } 
+}
